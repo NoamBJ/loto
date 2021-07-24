4d1919633807b0aff71076a91047e6e29b4c24da
Noam <noambires@gmail.com>
2021-07-24 14:21:08 +0200

    creer grilleLoto fonctionne et creer joueur aussi. CheckWinner fonctionne pas etne compte pas g et 2 joeurs ne peuvent pas entrer les mêmes nombres

--- loto.java
+++ loto.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class loto {
 
     Scanner in = new Scanner(System.in);
-    private int a;
+    private int a = in.nextInt();
 
     int[] array = new int[5];
     int[] grilleLoto = new int[5];
@@ -12,18 +12,12 @@ public class loto {
     private final int mise = a * 2;
     private int g = 0;
 
-    public void nbrJoueur() {
-        System.out.println("Combien de joueurs: ");
-        a = in.nextInt();
-    }
-
     public int num() {
         return a;
     }
 
     public void createPlayer() {
 
-        a = in.nextInt();
         in.nextLine();
 
         for (int i = 0; i < a; i++) {
@@ -44,7 +38,16 @@ public class loto {
 
     public int[] gridLoto() {
         for (int i = 0; i < grilleLoto.length; i++) {
-            grilleLoto[i] = (int) (Math.random() * 50);
+            grilleLoto[i] = (int) (Math.random() * 50 + 1);
+
+            for (int j = 0; j < grilleLoto.length; j++) {
+                if (i != j) {
+                    while (grilleLoto[i] == grilleLoto[j] || grilleLoto[i] < 1 || grilleLoto[i] > 50) {
+                        grilleLoto[i] = (int) (Math.random() * 50 + 1);
+                    }
+                }
+
+            }
         }
         return grilleLoto;
     }
@@ -71,37 +74,43 @@ public class loto {
             }
             if (g == 1) {
                 somme = mise / 32;
-                System.out.println("Bravo " + Joueur[i].getNom() + ", vous avez" + g
-                        + "nombre en commun avec la grille du loto et vous avez donc remporté" + somme + " €");
+                System.out.println("Bravo " + Joueur[i].getNom() + ", vous avez " + g
+                        + " nombre en commun avec la grille du loto et vous avez donc remporté " + somme + " €");
             } else if (g == 2) {
                 somme = mise / 16;
-                System.out.println("Bravo " + Joueur[i].getNom() + ", vous avez" + g
-                        + "nombre en commun avec la grille du loto et vous avez donc remporté" + somme + " €");
+                System.out.println("Bravo " + Joueur[i].getNom() + ", vous avez " + g
+                        + " nombre en commun avec la grille du loto et vous avez donc remporté " + somme + " €");
             } else if (g == 3) {
                 somme = mise / 8;
-                System.out.println("Bravo " + Joueur[i].getNom() + ", vous avez" + g
-                        + "nombre en commun avec la grille du loto et vous avez donc remporté" + somme + " €");
+                System.out.println("Bravo " + Joueur[i].getNom() + ", vous avez " + g
+                        + " nombre en commun avec la grille du loto et vous avez donc remporté " + somme + " €");
             } else if (g == 4) {
                 somme = mise / 4;
-                System.out.println("Bravo " + Joueur[i].getNom() + ", vous avez" + g
-                        + "nombre en commun avec la grille du loto et vous avez donc remporté" + somme + " €");
+                System.out.println("Bravo " + Joueur[i].getNom() + ", vous avez " + g
+                        + " nombre en commun avec la grille du loto et vous avez donc remporté " + somme + " €");
             } else if (g == 5) {
                 somme = mise / 2;
-                System.out.println("Bravo " + Joueur[i].getNom() + ", vous avez" + g
-                        + "nombre en commun avec la grille du loto et vous avez donc remporté" + somme + " €");
+                System.out.println("Bravo " + Joueur[i].getNom() + ", vous avez " + g
+                        + " nombre en commun avec la grille du loto et vous avez donc remporté " + somme + " €");
             }
         }
 
     }
 
     public static void main(String[] args) {
-
+        System.out.println("Combien de joueurs: ");
         loto Loto = new loto();
+        int a = Loto.num();
+
         System.out.println("Les numéros gagnats sont: ");
         Loto.gridLoto();
         Loto.afficheLoto();
 
-        Loto.createPlayer();
+        while (a > 0) {
+            Loto.createPlayer();
+            a--;
+        }
+
         Loto.checkWinner();
 
     }
